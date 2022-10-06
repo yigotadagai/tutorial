c6aaaf9ceb0d799ba543c14b5ecbd23852ddc739
robertv <1659391472@qq.com>
2022-10-06 15:55:13 +0800

    提交lock test代码

--- Java-senior/.idea/sonarlint/issuestore/index.pb
+++ Java-senior/.idea/sonarlint/issuestore/index.pb
@@ -1,9 +1,3 @@
 
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
-Z
-*src/main/java/com/chen/ThreadHomework.java,1/e/1eb0333e726efdb6b09c7e81c5cfb7af231a8de3
-V
-&src/main/java/com/chen/SaleTicket.java,f/3/f3504ffebae66ae10df9bda06155ac84040e2521
-X
-(src/main/java/com/chen/ThreadSocket.java,d/5/d53cb50a0a7c5c45a737d398f9811b5ffe214780
---
+++ Java-senior/src/main/java/com/chen/LockTest.java
@@ -0,0 +1,67 @@
+package com.chen;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author Robert V
+ * @create 2022-10-06-下午2:14
+ */
+public class LockTest {
+
+    private Integer ticket = 100;
+
+    private Lock lock = new ReentrantLock();
+
+    public void sale() {
+        lock.lock();
+        try {
+            if (ticket > 0) {
+                System.out.println(Thread.currentThread().getName() + ticket--);
+            } else {
+                System.out.println("票已卖完");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        LockTest lockTest = new LockTest();
+
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) {
+                lockTest.sale();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "thread-1: ").start();
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) {
+                lockTest.sale();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "thread-2: ").start();
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) {
+                lockTest.sale();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "thread-3: ").start();
+    }
+
+}
